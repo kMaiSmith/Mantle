b20ae0a9a9c16441b43955a848a74848b1d94b5e
Kyle Smith <kyle@kmaismith.com>
2023-06-27 21:24:54 -0400

    Fix GatherDataEvent import path

--- src/main/java/slimeknights/mantle/Mantle.java
+++ src/main/java/slimeknights/mantle/Mantle.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.data.event.GatherDataEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import slimeknights.mantle.block.entity.MantleSignBlockEntity;
